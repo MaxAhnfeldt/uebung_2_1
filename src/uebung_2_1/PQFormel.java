fb175dca9f85965f0da1b28b41ddd394213f6797
MaxAhnfeldt <maximilian.ahnfeldt@nordakademie.de>
2016-10-25 11:08:43 +0200

    blabla

--- src/uebung_2_1/PQFormel.java
+++ src/uebung_2_1/PQFormel.java
@@ -4,15 +4,25 @@ public class PQFormel {
 
 		public static void main(String[] args) {
 				double p=8;
-				double q=15;
+				double q=16;
 				
 				double unterDerWurzel = ((p/2)*(p/2))-q;
 				double vorDerWurzel = -1*p/2;
+		
+		if(unterDerWurzel <0) {
+			System.out.println("PQ-Formel ergibt keine Loesung.");
+		} else {
+			
+			if(unterDerWurzel ==0) {
+				System.out.println("x =" + vorDerWurzel);
+			} else {			
 				
 				double loesung1=vorDerWurzel + Math.sqrt(unterDerWurzel);
 				double loesung2=vorDerWurzel - Math.sqrt(unterDerWurzel);
 				
 				System.out.println("x1 = " + loesung1);
 				System.out.println("x2 = " + loesung2);
+					}
+				}
 		}
 }
